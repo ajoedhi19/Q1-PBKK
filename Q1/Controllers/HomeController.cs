b69e3829bdc83f74e2989e0a599e74daabb5c872
ajoedhi19 <ajoedhiawan@gmai.com>
2022-03-12 00:57:51 +0700

    First commit

---
+++ Q1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Q1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Company()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Collaboration()
+        {
+            return View();
+        }
+    }
+}
